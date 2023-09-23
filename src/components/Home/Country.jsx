d8709b88d0f99b2b50e32faa2c5cfb90e415c6d5
mehdi-sajedi <mehdiisajedi@gmail.com>
2023-09-22 17:08:02 -0700

    Updated country card animations from use-animate-presence to CSS keyframes

--- src/components/Home/Country.jsx
+++ src/components/Home/Country.jsx
@@ -1,39 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useAnimatePresence } from 'use-animate-presence';
-
-const variants = {
-  y: { from: 100, to: 0 },
-  opacity: { from: 0, to: 1 },
-};
 
 const Country = ({ alpha3Code, name, flag, population, region, capital }) => {
-  const animatedDiv = useAnimatePresence({
-    variants,
-    initial: 'hidden',
-  });
-
   const style = {
     backgroundImage: `url(${flag})`,
   };
 
   return (
-    <div ref={animatedDiv.ref} className={name}>
+    <div className={name}>
       <Link to={`/details/${alpha3Code}`}>
-        <div className="country">
-          <img className="country__flag" src={flag} alt="" />
-          <div className="country__info">
-            <h3 className="country__info__name">{name}</h3>
-            <ul className="country__info__details">
-              <li className="country__info__details__population">
+        <div className='country'>
+          <img className='country__flag' src={flag} alt='' />
+          <div className='country__info'>
+            <h3 className='country__info__name'>{name}</h3>
+            <ul className='country__info__details'>
+              <li className='country__info__details__population'>
                 <span>Population: </span>
                 {population.toLocaleString()}
               </li>
-              <li className="country__info__details__region">
+              <li className='country__info__details__region'>
                 <span>Region: </span>
                 {region || '-'}
               </li>
-              <li className="country__info__details__capital">
+              <li className='country__info__details__capital'>
                 <span>Capital: </span>
                 {capital || '-'}
               </li>
